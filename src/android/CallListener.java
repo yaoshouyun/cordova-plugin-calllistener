f179f8106c4faaa3692887c1e7e69dceb752f2ec
android <android>
2018-09-25 15:28:01 +0800

    v1.2.5

--- src/android/CallListener.java
+++ src/android/CallListener.java
@@ -11,6 +11,7 @@ import android.provider.CallLog;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.tbruyelle.rxpermissions.RxPermissions;
 
@@ -36,96 +37,107 @@ public class CallListener extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("callMobile".equals(action)) {
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    new RxPermissions(cordova.getActivity())
-                            .request(Manifest.permission.CALL_PHONE)
-                            .subscribe(new Action1<Boolean>() {
-                                @Override
-                                public void call(Boolean aBoolean) {
-                                    if (aBoolean) {
-                                        try {
-                                            String mobile = args.getString(0);
-                                            Intent intent = new Intent(Intent.ACTION_CALL);
-                                            Uri data = Uri.parse("tel:" + mobile);
-                                            intent.setData(data);
-                                            cordova.getActivity().startActivity(intent);
-                                        }catch (Exception e){
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }
-                            });
-                }
-            });
+            callMobile(args.getString(0));
             return true;
         } else if ("addListener".equals(action)) {
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    new RxPermissions(cordova.getActivity())
-                            .request(Manifest.permission.READ_PHONE_STATE, Manifest.permission.PROCESS_OUTGOING_CALLS)
-                            .subscribe(new Action1<Boolean>() {
-                                @Override
-                                public void call(Boolean aBoolean) {
-                                    if (aBoolean) {
-                                        CallListener.this.callbackContext = callbackContext;
-                                    }
-                                }
-                            });
-                }
-            });
+            addListener(callbackContext);
             return true;
         } else if ("getCallInfo".equals(action)) {
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    new RxPermissions(cordova.getActivity())
-                            .request(Manifest.permission.READ_CALL_LOG)
-                            .subscribe(new Action1<Boolean>() {
-                                @Override
-                                public void call(Boolean aBoolean) {
-                                    if (aBoolean) {
-                                        try {
-                                            String mobile = args.getString(0);
-                                            new Handler().postDelayed(new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    Cursor cursor = cordova.getContext().getContentResolver().query(CallLog.Calls.CONTENT_URI, new String[]{CallLog.Calls.DURATION}, CallLog.Calls.NUMBER + "=?", new String[]{mobile}, CallLog.Calls.DEFAULT_SORT_ORDER);
-                                                    int duration = 0;
-                                                    if (cursor.moveToFirst()) {
-                                                        duration = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.DURATION));
-                                                    }
-                                                    JSONObject object = new JSONObject();
-                                                    try {
-                                                        long startDate = endDate - duration * 1000;
-                                                        if (startDate < 0) {
-                                                            startDate = 0;
-                                                        }
-                                                        object.put("start", formaDatet(startDate));
-                                                        object.put("end", formaDatet(endDate));
-                                                        object.put("duration", duration);
-                                                    } catch (JSONException e) {
-                                                        e.printStackTrace();
-                                                    }
-                                                    callbackContext.success(object);
-                                                    endDate = 0;
-                                                }
-                                            }, 300);
-                                        } catch (Exception e) {
-                                            callbackContext.success(0);
-                                        }
-                                    } else {
-                                        callbackContext.success(0);
-                                    }
-                                }
-                            });
-                }
-            });
+            getCallInfo(args.getString(0),callbackContext);
             return true;
         }
         return false;
+    }
+
+    /* 拨打电话 */
+    private void callMobile(final String mobile) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new RxPermissions(cordova.getActivity())
+                        .request(Manifest.permission.CALL_PHONE)
+                        .subscribe(new Action1<Boolean>() {
+                            @Override
+                            public void call(Boolean aBoolean) {
+                                if (aBoolean) {
+                                    try {
+                                        Intent intent = new Intent(Intent.ACTION_CALL);
+                                        intent.setData(Uri.parse("tel:" + mobile));
+                                        cordova.getActivity().startActivity(intent);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    /* 添加电话状态监听 */
+    private void addListener(final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new RxPermissions(cordova.getActivity())
+                        .request(Manifest.permission.READ_PHONE_STATE, Manifest.permission.PROCESS_OUTGOING_CALLS)
+                        .subscribe(new Action1<Boolean>() {
+                            @Override
+                            public void call(Boolean aBoolean) {
+                                if (aBoolean) {
+                                    CallListener.this.callbackContext = callbackContext;
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    /* 获取通话信息 */
+    private void getCallInfo(final String mobile,final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new RxPermissions(cordova.getActivity())
+                        .request(Manifest.permission.READ_CALL_LOG)
+                        .subscribe(new Action1<Boolean>() {
+                            @Override
+                            public void call(Boolean aBoolean) {
+                                if (aBoolean) {
+                                    try {
+                                        new Handler().postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                CallInfo model = queryCallInfo(mobile);
+                                                if (Build.BRAND.equalsIgnoreCase("huawei") || Build.BRAND.equalsIgnoreCase("honor") || Build.BRAND.equalsIgnoreCase("meizu")) {
+                                                    if (model.duration > 0) {
+                                                        if (endDate - (model.date + model.duration * 1000) < 5000) {//如果5秒内电话未接通则认为没打通
+                                                            model.duration = 0;
+                                                        }
+                                                    }
+                                                }
+                                                JSONObject object = new JSONObject();
+                                                try {
+                                                    object.put("start", formaDatet(model.date));
+                                                    object.put("end", formaDatet(model.date + model.duration*1000));
+                                                    object.put("duration", model.duration);
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                callbackContext.success(object);
+                                                endDate = 0;
+                                            }
+                                        }, 1000);
+                                    } catch (Exception e) {
+                                        callbackContext.success(0);
+                                    }
+                                } else {
+                                    callbackContext.success(0);
+                                }
+                            }
+                        });
+            }
+        });
     }
 
 
@@ -184,6 +196,30 @@ public class CallListener extends CordovaPlugin {
             pluginResult.setKeepCallback(true);
             callbackContext.sendPluginResult(pluginResult);
         }
+    }
+
+    private CallInfo queryCallInfo(String mobile) {
+        CallInfo model = new CallInfo();
+        Cursor cursor = cordova.getContext().getContentResolver().query(CallLog.Calls.CONTENT_URI, new String[]{
+                CallLog.Calls.NUMBER,
+                CallLog.Calls.TYPE,
+                CallLog.Calls.DATE,
+                CallLog.Calls.DURATION
+        }, CallLog.Calls.NUMBER + "=?", new String[]{mobile}, CallLog.Calls.DEFAULT_SORT_ORDER);
+        if (cursor.moveToFirst()) {
+            model.number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
+            model.type = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE));
+            model.date = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE));
+            model.duration = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DURATION));
+        }
+        return model;
+    }
+
+    private class CallInfo {
+        public String number;//呼叫号码
+        public int type;//呼叫类型1来电、2去电、3未接、5拒接
+        public long date;//呼出时间
+        public long duration;//通话时长
     }
 
 }
